a4719cac2c09e3a86ce7b1a99a37f827a82ee41d
v_Vaishnavi.Saxena <v_vaishnavi.saxena@jsw.in>
2025-07-17 15:23:38 +0530

    changes

--- src/main/java/com/jswone/orchestrator/controller/OrderReleaseController.java
+++ src/main/java/com/jswone/orchestrator/controller/OrderReleaseController.java
@@ -8,13 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/job")
 public class OrderReleaseController {
 
   private final OrderReleaseService orderReleaseService;
