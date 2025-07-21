bec9d040dfa84253335f524ad6efcbc29d708141
v_Vaishnavi.Saxena <v_vaishnavi.saxena@jsw.in>
2025-07-21 14:03:05 +0530

    modify ExternalApi

--- src/main/java/com/jswone/orchestrator/config/ExternalApi.java
+++ src/main/java/com/jswone/orchestrator/config/ExternalApi.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "services")
+@ConfigurationProperties(prefix = "")
 public class ExternalApi {
 
   private Map<String, Map<String, String>> services;
